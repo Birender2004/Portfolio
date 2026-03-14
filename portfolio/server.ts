fe6f679494fbb9b5c6c211dc08d629fd420070d1
Birender2004 <143583792+Birender2004@users.noreply.github.com>
2026-03-14 20:53:47 +0530

    Update server.ts

--- portfolio/server.ts
+++ portfolio/server.ts
@@ -71,9 +71,12 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    app.use(express.static(path.join(__dirname, 'dist')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    const distPath = path.resolve(__dirname, '../dist');
+
+app.use(express.static(distPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
     });
   }
 
